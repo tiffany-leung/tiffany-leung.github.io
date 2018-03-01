02cfcc78055f6a1351b00e3bbf85b29578a5eab6
Tiffany Leung <leuns473@newschool.edu>
2018-03-01 18:52:30 -0500

    added

---
+++ Collections/javascript.js
@@ -0,0 +1,48 @@
+function myFunction() {
+    var Sostapopup = document.getElementById("Sosta-Popup");
+    Sostapopup.classList.toggle("show");
+}
+function myFunction2() {
+    var Spotpopup = document.getElementById("Spot-Popup");
+    Spotpopup.classList.toggle("show2");
+}
+function myFunction3() {
+    var Milkbarpopup = document.getElementById("Milkbar-Popup");
+    Milkbarpopup.classList.toggle("show3");
+}
+function myFunction4() {
+    var Milkbar2popup = document.getElementById("Milkbar2-Popup");
+    Milkbar2popup.classList.toggle("show4");
+}
+function myFunction5() {
+    var Ladureepopup = document.getElementById("Laduree-Popup");
+    Ladureepopup.classList.toggle("show5");
+}
+function myFunction6() {
+    var Beaconspopup = document.getElementById("Beacons-Popup");
+    Beaconspopup.classList.toggle("show6");
+}
+function myFunction7() {
+    var MJpopup = document.getElementById("MJ-Popup");
+    MJpopup.classList.toggle("show7");
+}
+function myFunction8() {
+    var Glossierpopup = document.getElementById("Glossier-Popup");
+    Glossierpopup.classList.toggle("show8");
+}
+function myFunction9() {
+    var MKpopup = document.getElementById("MK-Popup");
+    MKpopup.classList.toggle("show9");
+}
+function myFunction10() {
+    var Citycakespopup = document.getElementById("Citycakes-Popup");
+    Citycakespopup.classList.toggle("show10");
+}
+function myFunction11() {
+    var Foragerspopup = document.getElementById("Foragers-Popup");
+    Foragerspopup.classList.toggle("show11");
+}
+function myFunction12() {
+    var Tamoripopup = document.getElementById("Tamori-Popup");
+    Tamoripopup.classList.toggle("show12");
+}
