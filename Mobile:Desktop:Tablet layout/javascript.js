dfadc7e85fee1191ec8fcef9acc6d153a5f9c083
Tiffany Leung <leuns473@newschool.edu>
2018-04-18 18:04:37 -0400

    added

---
+++ Mobile:Desktop:Tablet layout/javascript.js
@@ -0,0 +1,47 @@
+//gallery 1
+document.querySelector("#one").onclick = function () {
+  document.querySelector("#one").style.display = "none";
+  document.querySelector("#two").style.display = "block";
+}
+document.querySelector("#two").onclick = function () {
+  document.querySelector("#two").style.display = "none";
+  document.querySelector("#three").style.display = "block";
+}
+document.querySelector("#three").onclick = function () {
+  document.querySelector("#three").style.display = "none";
+  document.querySelector("#four").style.display = "block";
+}
+document.querySelector("#four").onclick = function () {
+  document.querySelector("#four").style.display = "none";
+  document.querySelector("#five").style.display = "block";
+}
+document.querySelector("#five").onclick = function () {
+  document.querySelector("#five").style.display = "none";
+  document.querySelector("#six").style.display = "block";
+}
+document.querySelector("#six").onclick = function () {
+  document.querySelector("#six").style.display = "none";
+  document.querySelector("#seven").style.display = "block";
+}
+document.querySelector("#seven").onclick = function () {
+  document.querySelector("#seven").style.display = "none";
+  document.querySelector("#one").style.display = "block";
+}
+
+//gallery 2
+document.querySelector("#T1").onclick = function () {
+  document.querySelector("#T1").style.display = "none";
+  document.querySelector("#T2").style.display = "block";
+}
+document.querySelector("#T2").onclick = function () {
+  document.querySelector("#T2").style.display = "none";
+  document.querySelector("#T3").style.display = "block";
+}
+document.querySelector("#T3").onclick = function () {
+  document.querySelector("#T3").style.display = "none";
+  document.querySelector("#T4").style.display = "block";
+}
+document.querySelector("#T4").onclick = function () {
+  document.querySelector("#T4").style.display = "none";
+  document.querySelector("#T1").style.display = "block";
+}
